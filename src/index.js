0f1207883ea36fb746f3cc671382d31cea6cfb9b
DaniloAp <daniloap.cavicchiolli@gmail.com>
2023-03-26 20:41:58 -0300

    add: context para usuário

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./styles/global.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 import App from './views/pages/SignIn';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ToastContainer />
     <App />
   </React.StrictMode>
 );
